4e54f26d17d2a03b3304e7dd45b024ca3eff9ee1
Brooke Snader <brookesnader@Brookes-Mac-mini-2.local>
2026-05-30 23:00:50 -0400

    Publish Olara editorial showcase page

--- src/main.ts
+++ src/main.ts
@@ -7547,6 +7547,7 @@ function berkeleyIcon(name: string) {
     bed: '<path d="M4 18V7"/><path d="M20 18v-5a3 3 0 0 0-3-3H4"/><path d="M4 13h16"/><path d="M8 10V8h4v2"/>',
     status: '<path d="M4 19V5"/><path d="M4 7h12l-2 4 2 4H4"/><path d="M19 19H3"/>',
     pool: '<path d="M4 16c2 0 2 1.5 4 1.5s2-1.5 4-1.5 2 1.5 4 1.5 2-1.5 4-1.5"/><path d="M7 12V5"/><path d="M7 5h6a3 3 0 0 1 3 3v4"/>',
+    waterfront: '<path d="M3 17c1.5 0 1.5 1 3 1s1.5-1 3-1 1.5 1 3 1 1.5-1 3-1 1.5 1 3 1 1.5-1 3-1"/><path d="m5 14 2-5h10l2 5"/><path d="M9 9V5h6v4"/><path d="M12 5V3"/>',
     cabana: '<path d="M4 11c2.5-4 13.5-4 16 0"/><path d="M12 7v12"/><path d="M6 19h12"/><path d="M8 13h8"/>',
     fitness: '<path d="M6 8v8"/><path d="M18 8v8"/><path d="M3 10v4"/><path d="M21 10v4"/><path d="M6 12h12"/>',
     spa: '<path d="M12 19c4-3 6-6 6-10-4 0-6 2-6 6 0-4-2-6-6-6 0 4 2 7 6 10Z"/><path d="M12 19v-6"/>',
