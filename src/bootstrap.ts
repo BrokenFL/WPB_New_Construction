b7deaf207081bb19dabff8db5bcc393174b8a166
BrokenFL <128734086+BrokenFL@users.noreply.github.com>
2026-09-08 13:19:05 -0400

    Wire Batch 4 buyer guides into existing bootstrap

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,6 +3,7 @@ import "./style.css";
 import "./floorplanEntities.css";
 import "./commercialGrowth.css";
 import "./shortlistSummary.css";
+import "./projectSeoBatch4.css";
 import { wireInquiryContext } from "./lib/inquiryContext.ts";
 import { cleanFloorplanPath, mergeFloorplanDiscoverySchema, floorplanForPath, floorplanJson, renderFloorplanDiscovery } from "./lib/floorplanEntities.ts";
 
@@ -31,6 +32,8 @@ async function start() {
   installCorridorGrowth();
   const { installComparisonDiscovery } = await import('./comparisonDiscovery.ts');
   installComparisonDiscovery(app);
+  const { installProjectSeoBatch4 } = await import('./projectSeoBatch4.ts');
+  await installProjectSeoBatch4(app);
 
   // Entity routes are full document navigations, outside the legacy router.
   // Preserve native middle/modified clicks and no-JavaScript crawlable anchors.
